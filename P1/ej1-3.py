1bd87caa8d9f803641172612cc807c7e78bb7503
Alberto <aandgom@etsinf.upv.es>
2024-11-25 19:02:33 +0100

    sd

--- P1/ej1-3.py
+++ P1/ej1-3.py
@@ -2,11 +2,36 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-img_fondo1 = Image.open("Fondo1.tif")
-img_fondo2 = Image.open("Fondo2.tif")
+img_fondo1 = Image.open("azulejo.tif")
+img_fondo2 = Image.open("azulejo2.tif")
 
 # Redimensionar las dos imagenes a 256x256 
 img_fondo1_resized = img_fondo1.resize((256, 256))
 img_fondo2_resized = img_fondo2.resize((256, 256))
-#img_fondo1_resized.show()
-#img_fondo2_resized.show()
+
+# Realización de diferentes operaciones con las imágenes
+# Suma de las dos imágenes
+#img_suma = Image.blend(img_fondo1_resized, img_fondo1_resized, 0.5)
+#img_suma.show()
+
+# Resta de las dos imágenes
+#img_resta = Image.blend(img_fondo1_resized, img_fondo2_resized, 0.5)
+#img_resta.show()
+
+# Realización de la combinación lineal
+# Convertir las imágenes a arrays de NumPy
+img_fondo1_array = np.array(img_fondo1_resized, dtype=np.float32)
+img_fondo2_array = np.array(img_fondo2_resized, dtype=np.float32)
+
+# Realización de la combinación lineal
+# azulejo * 1.8 - azulejo2 * 1.2 + 128
+img_combined_array = (img_fondo1_array * 1.8) - (img_fondo2_array * 1.2) + 128
+
+# Asegurarse de que los valores estén en el rango [0, 255]
+img_combined_array = np.clip(img_combined_array, 0, 255).astype(np.uint8)
+
+# Convertir el array de vuelta a una imagen
+img_combined = Image.fromarray(img_combined_array)
+
+img_combined.show()
+
--- P1/p1.py
+++ P1/p1.py
@@ -2,17 +2,33 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Cargar la imagen
+img = Image.open("d52.jpg")
+"""
+#EJERCICIO 1
+# Mostrar la imagen original
+img.show()
+
+# Convertir la imagen a un formato con 16 colores
+img_reduced = img.convert("P", palette=Image.ADAPTIVE, colors=4)
+
+# Mostrar la imagen reducida
+img_reduced.show()
+
+
 #EJERCICIO 2
+img_reduced = img.convert("P", palette=Image.ADAPTIVE, colors=4)
+
 # Imagen con rotación de 90 grados y volteada horizontalmente
 img_transformed = img_reduced.rotate(90).transpose(Image.FLIP_LEFT_RIGHT)
-#img_transformed.show()
+img_transformed.show()
 
 # Convertir imagen de color a grises
 img_2 = Image.open("cartas_4.jpg")
 img_gray = img_2.convert("L")
-#img_2.show()
-#img_gray.show()
-
+img_2.show()
+img_gray.show()
+"""
 # Mostrar los tres componentes RGB de la imagen como imágenes de gris por separado
 img_aloe = Image.open("AloeVera.jpg")
 r, g, b = img_aloe.split()
